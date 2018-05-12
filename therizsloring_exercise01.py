e5d6d06eacc0974b40f8ddfee35a4ebc7a067e1c
Theriz L <sage.thrz@gmail.com>
2018-05-12 14:15:07 +0800

    added challenge description

--- therizsloring_exercise01.py
+++ therizsloring_exercise01.py
@@ -1,3 +1,10 @@
+""" The Challenge
+Author:
+Description: Aling Nena’s Sari-sari store wants a robot that will ask the
+customer their total bill and payment amount and tell them their change
+(for now, we can allow negative change).
+"""
+
 Bill = input('Total Bill:')
 Pay = input('Enter your cash amount:')
 Change = float(Pay) - float(Bill)
